0b08a9a4c5b575e3003ae90d150603e642a841ab
fuck you and your fucking shit man <a231892@trbvm.com>
2015-03-30 06:51:58 -0700

    Update tcqpBot.py

--- tcqpBot.py
+++ tcqpBot.py
@@ -3,11 +3,10 @@
 # EXAMPLE SCRIPT FOR TINYCHAT BOT
 # >https://github.com/alyak/pytiny
 
-# tcqpBot.py DEFAULT PROTOTYPE
-# SINGLE BOT / Posts random quotes from randomquotesgenerator.com
-#              and occasionally posts <pic> from Tinychat .xml
+# tc_bot0000.py DEFAULT PROTOTYPE
+# BRINGS IN 9 BOTS, EACH SPEAK WHEN ENTERING, 9TH BOT WILL CONTINUE SPEAKING.
 
-# NOTE: Line 125 & 140 of tinychat.py (#)commented out, or might crash.
+# NOTE: Line 125 of tinychat.py (#)commented out, or doesnt't work.
 # 2015 ~k
 
 # YOU MAY NEED TO DO THE FOLLOWING FIRST [Debian]:
@@ -19,54 +18,51 @@
 # 	>http://pythonhosted.org/python-librtmp
 
 import pytiny
-import time, re, random, urllib, urllib2
+import time, socket, re, random, urllib, urllib2
 from urllib import urlencode
 from urllib2 import urlopen, Request
 
 proxy_support = urllib2.ProxyHandler({"https" : "127.0.0.1:8118"})
 opener = urllib2.build_opener(proxy_support) # apt-get install privoxy
 
-VOWELS = "aeiou"
-CONSONANTS = "bcdfghjklmnpqrstvwxyz"
-def generate_nick(length):
-	word = ""
-	for i in range(length):
-		if i % 2 == 0:
-			word += random.choice(CONSONANTS)
-		else:
-			word += random.choice(VOWELS)
-	return word
-if __name__ == "__main__":
-	count = random.randint(3, 6)
-	length = count + 1
-	for i in range(count):
-		rnick = (generate_nick(length))
-		
-tc = pytiny.TinyChat()
-tc.nick(rnick)
-tc.set_room("scenefag")
-tc.connect()
-	
-while True:
-	buf = tc.recv()
-	print buf
-	stfu = random.randint(45, 80)
-	getqchoice = random.randint(0, 1)
-	if getqchoice == 0:
-		getquote = 'http://api.tinychat.com/scenefag.xml'
-		rquote = re.compile(r'<pic>.*?</pic>')
-		qdata = opener.open(getquote).read()
-		qpick = rquote.findall(qdata)
-		pic = re.sub(r'<[^>]*?>', '', unicode(qpick[0]))
-		ohai = '*Say cheese!* ' + pic
-		if ohai == '':
+SEED_ = 9
+    
+while not SEED_ <= 0:
+	VOWELS = "aeiou"
+	CONSONANTS = "bcdfghjklmnpqrstvwxyz"
+	def generate_nick(length):
+		word = ""
+		for i in range(length):
+			if i % 2 == 0:
+				word += random.choice(CONSONANTS)
+			else:
+				word += random.choice(VOWELS)
+		return word
+	if __name__ == "__main__":
+		count = random.randint(3, 5)
+		length = count + 1
+		for i in range(count):
+			rnick = (generate_nick(length))
+	tc = pytiny.TinyChat()
+	tc.nick(rnick)
+	tc.set_room("scenefag")
+	tc.connect()
+	data = tc.recv()
+	data_string = str(data).lower()
+	print data
+	stfu = random.randint(10, 60)
+	tc.msg("*WHITEPOWER!*", "#0")
+	time.sleep(stfu)
+	SEED_ = SEED_ - 1
+	if SEED_ <= 0:
+		while True:
+			buf = tc.recv()
+			print buf
+			stfu = random.randint(180, 600) # Every 3-10 mins.
 			getquote = 'http://randomquotesgenerator.com/index.php/rest.html'
 			rquote = re.compile(r'<i>.*?</i>')
-	elif getqchoice == 1:
-		getquote = 'http://randomquotesgenerator.com/index.php/rest.html'
-		rquote = re.compile(r'<i>.*?</i>')
-	qdata = opener.open(getquote).read()
-	qpick = rquote.findall(qdata)
-	ohai = re.sub(r'<[^>]*?>', '', unicode(qpick[0]))
-	tc.msg(ohai, "#0")
-	time.sleep(stfu)
+			qdata = opener.open(getquote).read()
+			qpick = rquote.findall(qdata)
+			ohai = re.sub(r'<[^>]*?>', '', unicode(qpick[0]))
+			tc.msg(ohai)
+			time.sleep(stfu)
